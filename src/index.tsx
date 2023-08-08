0238ee3c2aec8288719edccde2327a46aa6be369
byrmylmz <bayramyilmaz061@gmail.com>
2023-08-08 12:19:18 +0300

    Bootstrap removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-import Integration from "./Component/integration";
-import Room from "./Component/room";
-import Players from "./Component/players";
-import Settings from "./Component/settings";
-import Roomlist from "./Component/Room/roomlist";
+import Roomlist from "./components/Room/roomlist";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 const router = createBrowserRouter([
 
-  {
-    path: "/",
-    element: <App />,
-    children: [
-       {
-        path: "/integration",
-        element: <Integration/>,
-      },
-      {
-        path: "/room",
-        element: <Room/>,
-      },
-      {
-        path: "/room/:id",
-        element: <Room/>,
-      },
-      {
-        path: "/roomlist",
-        element: <Roomlist/>,
-      },
-      {
-        path: "/settings",
-        element: <Settings/>,
-      },
-      {
-        path: "/players",
-        element: <Players/>,
-      },
-    ],
-  },
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+
+
+            {path: "/roomlist", element: <Roomlist/>,},
+
+        ],
+    },
 ]);
+
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 
-root.render(<RouterProvider router={router} />);
+root.render(
+    <Provider store={store}>
+        <RouterProvider router={router}/>
+    </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
