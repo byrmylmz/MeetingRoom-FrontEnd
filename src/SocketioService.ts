a12c2c4db7570c63cf147df62d04845f11589189
byrmylmz <bayramyilmaz061@gmail.com>
2024-01-25 11:48:15 +0100

    Reserved and Available works Book now Button and modal works

--- src/SocketioService.ts
+++ src/SocketioService.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from 'socket.io-client';
 
-// const socket: Socket = io('ws://localhost:8085/calendar');
-const socket: Socket = io('https://screenapi.mintyfi.com/calendar');
+const socket: Socket = io('ws://localhost:8085/calendar');
+// const socket: Socket = io('https://screenapi.mintyfi.com/calendar');
 
 
 export default socket;
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         createProxyMiddleware('/api',{
-            target: 'https://screenapi.mintyfi.com',
+            // target: 'https://screenapi.mintyfi.com',
+            target: 'http://localhost:8082',
             changeOrigin: true,
         })
     );
